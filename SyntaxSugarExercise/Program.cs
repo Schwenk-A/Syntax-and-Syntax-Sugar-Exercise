bc624cb735d0677323a1220ea6c97936db57c355
Schwenk-A <100094451+Schwenk-A@users.noreply.github.com>
2022-08-29 14:55:43 -0500

    Creating Template

---
+++ SyntaxSugarExercise/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SyntaxSugarExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
